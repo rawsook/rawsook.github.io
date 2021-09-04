585a3ce89098d8ad3f2ac8209c3c20a3fd0dde1b
rawsook <sukronsukran207@gmail.com>
2021-09-04 12:27:11 +0700

    membuat awal

---
+++ js/my.js
@@ -0,0 +1,13 @@
+// $('.page-scroll').on('click', function(e){
+
+//     var tujuan = $(this).attr('href');
+
+//     var elementujuan = $(tujuan);
+
+//     // console.log(elementujuan.offset().top);
+//     // console.log($('body').scrolltop());
+//     console.log($('body').scrollTop());
+//     $('body').scrollTop('0px');
+
+//     e.preventDefault();
+// });
